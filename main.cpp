34af32fefd5e61526c85e5f45e2eb1bf472d3a4d
Alice (YakuninEM) <alice@example.com>
2020-10-11 20:37:50 +0300

    code:замена int на double

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-    int a, b;
+    double a, b;
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
@@ -18,14 +18,20 @@ int main()
     poisk[2]=a*b;
     poisk[3]=a/b;
     double max=poisk[0];
+    double min=poisk[0];
     for(size_t i=0; i<3; i++)
     {
         if (max<poisk[i+1])
         {
             max=poisk[i+1];
         }
+        if (min>poisk[i + 1])
+        {
+            min=poisk[i + 1];
+        }
     }
     cout <<"max="<<max<<'\n';
+    cout << "min=" << min << '\n';
 
     return 0;
 }
