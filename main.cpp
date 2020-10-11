83d12be3e08c7fb836adc2e5e167b205055f55b9
Alice (YakuninEM) <alice@example.com>
2020-10-11 20:19:20 +0300

    code:поиск максимума от Алисы

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -11,5 +12,20 @@ int main()
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n';
+    vector <double> poisk(4);
+    poisk[0]=a+b;
+    poisk[1]=a-b;
+    poisk[2]=a*b;
+    poisk[3]=a/b;
+    double max=poisk[0];
+    for(size_t i=0; i<3; i++)
+    {
+        if (max<poisk[i+1])
+        {
+            max=poisk[i+1];
+        }
+    }
+    cout <<"max="<<max<<'\n';
+
     return 0;
 }
